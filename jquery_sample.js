1d8fed40f07138c438cf979862d3013b49e11fd8
purnasrivatsa96 <purnasrivatsa96@gmail.com>
2017-06-16 12:02:11 +0530

    Add files via upload

---
+++ jquery_sample.js
@@ -0,0 +1,20 @@
+var $ = require('jquery');
+var http = require('http');
+
+var options = {
+    host: 'jquery.com',
+    port: 80,
+    path: '/'
+};
+
+var html = '';
+http.get(options, function(res) {
+    res.on('data', function(data) {
+        // collect the data chunks to the variable named "html"
+        html += data;
+    }).on('end', function() {
+        // the whole of webpage data has been collected. parsing time!
+        var title = $(html).find('title').text();
+        console.log(title);
+     });
+});
